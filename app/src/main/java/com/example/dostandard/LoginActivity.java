d87c451e8b7c6fcd37411f9a1dda123901bc5680
unknown <kys00514@skku.edu>
2023-03-14 13:50:11 +0900

    stringadded

--- app/src/main/java/com/example/dostandard/LoginActivity.java
+++ app/src/main/java/com/example/dostandard/LoginActivity.java
@@ -3,10 +3,19 @@ package com.example.dostandard;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.Typeface;
 import android.os.Bundle;
+import android.text.Spannable;
+import android.text.SpannableString;
+import android.text.style.ForegroundColorSpan;
+import android.text.style.RelativeSizeSpan;
+import android.text.style.StyleSpan;
+import android.util.Log;
 import android.view.Menu;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -14,7 +23,19 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-
+        TextView text=findViewById(R.id.welcometext);
+        String content = text.getText().toString(); //텍스트 가져옴.
+        SpannableString spannableString = new SpannableString(content); //객체 생성
+        String word ="감탄프로젝트: COlour";
+        int start = content.indexOf(word);
+        int end = start + word.length();
+        Log.d("asdf",Integer.toString(start));
+        Log.d("asdf",Integer.toString(end));
+        spannableString.setSpan(new StyleSpan(Typeface.BOLD), start, end, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+        spannableString.setSpan(new ForegroundColorSpan(Color.parseColor("#2b5d5b")), start, end, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+        spannableString.setSpan(new StyleSpan(Typeface.BOLD), start, end, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+        spannableString.setSpan(new RelativeSizeSpan(1.3f), start, end, SpannableString.SPAN_EXCLUSIVE_EXCLUSIVE);
+        text.setText(spannableString);
     }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
