a4cc59d1f190bca5f1e71b5abe16b76b4474eecb
unknown <kys00514@skku.edu>
2023-03-14 16:05:30 +0900

    main

--- app/src/main/java/com/example/dostandard/MainActivity.java
+++ app/src/main/java/com/example/dostandard/MainActivity.java
@@ -16,7 +16,26 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        TableLayout sv = (TableLayout) findViewById(R.id.categoryLinearLayout1);
 
+        for (int i = 0; i < 2; i++) {
+            TableRow tr = new TableRow(this);
+            tr.setLayoutParams(new TableLayout.LayoutParams(TableLayout.LayoutParams.FILL_PARENT, TableLayout.LayoutParams.WRAP_CONTENT));
+            for (int j = 0; j < 2; j++) {
+                ImageButton ib = new ImageButton(this);
+                // ib.setImageDrawable(getResources().getDrawable(R.drawable.cat1));
+                Bitmap bmp = BitmapFactory.decodeResource(getResources(),
+                        R.drawable.img1);
+                int width = 300;
+                int height = 300;
+                Bitmap resizedbitmap = Bitmap.createScaledBitmap(bmp, width,
+                        height, true);
+                ib.setImageBitmap(resizedbitmap);
+                ib.setLayoutParams(new TableRow.LayoutParams(TableRow.LayoutParams.WRAP_CONTENT, TableRow.LayoutParams.WRAP_CONTENT));
+                tr.addView(ib);
+            }
+            sv.addView(tr);
+        }
 
 
 
